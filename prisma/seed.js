1532df867eeacc60b2d03cddbb7ac98d36678c73
Imad <imadchowdhury01@gmail.com>
2025-05-24 10:19:05 +0100

    Add Prisma setup with initial migration and seed data

---
+++ prisma/seed.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const taskData = [
+	{ title: 'task 1', description: 'this is task 1' },
+	{ title: 'task 2', description: 'this is task 2' },
+	{ title: 'task 3', description: 'this is task 3' }
+];
+
+async function main() {
+	for (const task of taskData) {
+		await prisma.task.create({
+			data: {
+				title: task.title,
+				description: task.description
+			}
+		});
+	}
+	console.log('Seeding complete.');
+}
+
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
